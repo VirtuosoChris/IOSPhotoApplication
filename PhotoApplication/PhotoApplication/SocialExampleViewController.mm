12a2f20ba37be6b173b97cb4c36b348f05afd859
ChrisPugh666 <ChrisPugh666@0e5c7355-8765-436c-a58d-96fe7be22c8b>
2015-08-29 00:27:27 +0000

    Added copyright banners to files where they were missing

--- PhotoApplication/PhotoApplication/SocialExampleViewController.mm
+++ PhotoApplication/PhotoApplication/SocialExampleViewController.mm
@@ -1,4 +1,11 @@
 //
+// Copyright 2015 Lumialis LLC
+//
+// Licensed under the TrackingTeam License, Version 1.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at https://tldrlegal.com/license/trackingteam-licence#fulltext
+//
+
+//
 //  SocialExampleViewController.m
 //
 
